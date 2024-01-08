426afb93959b2be5ee6db1767bdf7440f8e8cb3e
Taeseong-eom <6380714@gmail.com>
2024-01-08 21:12:30 +0900

    인스타그램 자동로그인

--- Crawling/Part2/InstagramBot/InstagramBot1.py
+++ Crawling/Part2/InstagramBot/InstagramBot1.py
@@ -25,11 +25,22 @@ driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install())
 driver.get('https://instagram.com') # 드라이버에는 사이트에 관한 모든 정보가 들어가게 됨
 time.sleep(5) # 5초 정지(페이지 뜨는데 시간이 걸려서 기다림)
 
-e = driver.find_element(By.CLASS_NAME, '_ap3a').text
-print(e)# 가입하기 글자 출력완.
+# e = driver.find_element(By.CLASS_NAME, '_ap3a').text
+# print(e)# 가입하기 글자 출력완.
+
+# e = driver.find_element(By.CLASS_NAME, 'x9f619').text
+# print(e)# 로그인 글자?
 
 
+# e = driver.find_element(By.NAME, 'username').text
+# print(e)
 
 
+#인스타그램 자동로그인 해보자
+e = driver.find_element(By.NAME, 'username') # 여기에 아이디 입력할 거임
+e.send_keys('djaxotjd95@naver.com')
+e = driver.find_element(By.NAME, 'password') # 여기에 비밀번호 입력할 거임
+e.send_keys('비밀번호')
+e.send_keys(Keys.ENTER) # 이렇게 엔터도 칠 수 있음
 
 time.sleep(1000000) #코드 맨 마지막에 추가/ 자동 종료 방지
