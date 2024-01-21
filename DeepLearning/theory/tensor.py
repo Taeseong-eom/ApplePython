4fbbb3832cca5473445dc77f3c6ba2f3eadf3a3a
Taeseong-eom <6380714@gmail.com>
2024-01-21 23:29:05 +0900

    tensorflow gpu 설정

---
+++ DeepLearning/theory/tensor.py
@@ -0,0 +1,16 @@
+import tensorflow as tf
+
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+  try:
+    # Currently, memory growth needs to be the same across GPUs
+    for gpu in gpus:
+      tf.config.experimental.set_memory_growth(gpu, True)
+    logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+    print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+  except RuntimeError as e:
+    # Memory growth must be set before GPUs have been initialized
+    print(e)
+    
+텐서 = tf.constant([113,4214,22])
+print(텐서)
